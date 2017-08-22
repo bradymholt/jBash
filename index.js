1dfaef3a3ce15d8f788a7d52fa8066d9d81bdd15
Brady Holt <brady.holt@gmail.com>
2017-08-22 08:44:15 -0500

    Only slice 3 args

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // jbash
-args = process.argv.slice(4); cd = process.chdir; exit = process.exit; env = process.env; echo = console.log;
+args = process.argv.slice(3); cd = process.chdir; exit = process.exit; env = process.env; echo = console.log;
 encoding_default = "utf-8"; _fs = require("fs"); readFile = (path, encoding = encoding_default) => { return _fs.readFileSync(path, { encoding }) }; writeFile = (path, contents, encoding = encoding_default) => { _fs.writeFileSync(path, contents, { encoding }) };
 $ = (cmd, stream) => { r = (require("child_process").execSync(cmd, { stdio: stream ? "inherit" : "pipe" }) ); return !!r ? r.toString().replace(/^\n$/, "") : null; };
 eval = cmd => { return $(cmd, true); };
