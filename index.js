aa6706494bd736d5a371ba4cda8fff0c62bc7299
Brady Holt <brady.holt@gmail.com>
2017-08-21 22:34:23 -0500

    Rename $ to eval

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 // jsbash
 args = process.argv.slice(2); cd = process.chdir; exit = process.exit; env = process.env; echo = console.log;
 encoding_default = "utf-8"; _fs = require("fs"); readFile = (path, encoding = encoding_default) => { return _fs.readFileSync(path, { encoding }) }; writeFile = (path, contents, encoding = encoding_default) => { _fs.writeFileSync(path, contents, { encoding }) };
-$$ = (cmd, stream) => { r = (require("child_process").execSync(cmd, { stdio: stream ? "inherit" : "pipe" }) ); return !!r ? r.toString().replace(/^\n$/, "") : null; };
-$ = cmd => { return $(cmd, true); };
+$ = (cmd, stream) => { r = (require("child_process").execSync(cmd, { stdio: stream ? "inherit" : "pipe" }) ); return !!r ? r.toString().replace(/^\n$/, "") : null; };
+eval = cmd => { return $(cmd, true); };
