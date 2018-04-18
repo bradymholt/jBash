1fc84e9be358d0d5f4497a310cf669139b0755a6
Brady Holt <brady.holt@gmail.com>
2018-04-18 17:49:53 -0500

    Shebang fix for relative paths

--- bin/jbash.js
+++ bin/jbash.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 const path = require("path");
-const script = path.join(process.cwd(), process.argv[2]);
+let script = path.join(process.argv[2]);
+
+if (!process.argv[2].startsWith("/")) {
+  // Relative path so join with current working directory
+  script = path.join(process.cwd(), process.argv[2]);
+}
 
 global.shebangInvoked = true;
 
