1c9062e880e67a2c6c59eb26989acf2a91048cb5
Brady Holt <brady.holt@gmail.com>
2019-11-19 11:56:28 -0600

    Formatting

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ global.set = opt => {
 // Aliases
 global.args = process.argv.slice(global.shebangInvoked ? 3 : 2);
 // Current filename aliased as $0
-const path = require('path');
+const path = require("path");
 global[`$0`] = global.scriptName || path.basename(module.parent.filename);
 // Arguments aliased as $1, $2, etc.
 // $1 through $10, at a minimum, will be declared and have argument value or be set to undefined if not specified
@@ -49,9 +49,9 @@ global.echo = (contents, outPath, encoding) => {
   if (outPath === undefined) {
     console.log(contents);
   } else {
-    global.writeFile(outPath, contents, encoding)
+    global.writeFile(outPath, contents, encoding);
   }
-}
+};
 
 // Command execution
 global["$"] = (cmd, stream) => {
@@ -72,7 +72,7 @@ global["$"] = (cmd, stream) => {
 
     global.echo(msg);
 
-    if (global["options"].errexit) {
+    if (global.options.errexit) {
       err = new Error(msg);
       err.status = result.status;
       err.stderr = stderr;
