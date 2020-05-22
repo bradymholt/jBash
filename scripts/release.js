05a8ed2b37d13ad86455c6a9b8f35c135016019f
Brady Holt <brady.holt@gmail.com>
2020-05-22 16:34:52 -0500

    Add link to project in output

--- scripts/release.js
+++ scripts/release.js
@@ -1,25 +1,31 @@
 #!bin/jbash-local.js
 
-set("-e")
+set("-e");
 
-cd(`${__dirname}/../`)
+cd(`${__dirname}/../`);
 
 // Bump version
-eval(`npm version --no-git-tag-version minor`)
+eval(`npm version --no-git-tag-version minor`);
 
 // Prefix source with jbash - v0.0.0
-let package = require("../package.json")
+let package = require("../package.json");
 let sourceFile = "index.js";
-let sourceContent = readFile(sourceFile)
+let sourceContent = readFile(sourceFile);
 // Remove first line from source
 sourceContent = sourceContent.split("\n").slice(1).join("\n");
 // Prefix it
-writeFile(sourceFile, `// ${package.name} - v${package.version}\n${sourceContent}`)
+writeFile(
+  sourceFile,
+  `\
+// ${package.name} - v${package.version}\n${sourceContent}\
+// ${package.homepage}
+`
+);
 
 // Git commit/tag
-eval(`git commit -a -m "New version: ${package.version}"`)
-eval(`git tag ${package.version}`)
-eval(`git push --tags`)
+eval(`git commit -a -m "New version: ${package.version}"`);
+eval(`git tag ${package.version}`);
+eval(`git push --tags`);
 
 // Publish
-eval(`npm publish`)
+eval(`npm publish`);
